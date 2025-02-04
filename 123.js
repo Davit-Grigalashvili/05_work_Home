e7418fad9110a4ee385ad6dd92f6038f5ba60af5
davit-grigalashvili <grigalashvili.davit@gmail.com>
2025-02-05 00:05:21 +0400

    sad

---
+++ 123.js
@@ -0,0 +1,175 @@
+//
+// let a = 3 + 2;
+//
+// switch (a) {
+//     case 3:
+//         console.log( 'Too small' );
+//         break;
+//     case 4:
+//         console.log( 'Exactly!' );
+//         break;
+//     case 5:
+//         console.log( 'Too big' );
+//         break;
+//     default:
+//         console.log( "I don't know such values" );
+// }
+
+
+// let i = 3 * 6 * 2;
+//
+// switch (i) {
+//     case 18:
+//         console.log( 'Too small' );
+//
+//     case 36:
+//         console.log( 'Exactly!' );
+//
+//     case 65:
+//         console.log( 'Too big' );
+//
+//     default:
+//         console.log( "I don't know such values" );
+// }
+
+// let i = 3 * 6 * 2;
+//
+// switch (i) {
+//     case i > 18:
+//         console.log( 'Too small' );
+//
+//     case i > 36:
+//         console.log( 'Exactly!' );
+//
+//     case i < 37:
+//         console.log( 'Too big' );
+//
+//     default:
+//         console.log( "I don't know such values" );
+// }
+
+// let a = "1";
+// let b = 0;
+//
+// switch (+a) {
+//     case b + 1:
+//         console.log("this runs, because +a is 1, exactly equals b+1");
+//         break;
+//
+//     default:
+//         console.log("this doesn't run");
+// }
+
+// let a = 3;
+//
+// switch (a) {
+//     case 4:
+//         console.log('Right!');
+//
+//     // case 3:
+//     //     console.log("all is ok");
+//     //     break;
+//
+//     case 5:
+//         console.log('Wrong!');
+//
+//
+//     default:
+//         console.log('The result is strange. Really.');
+// }
+
+// let a = 3;
+//
+// switch (a) {
+//     case 3:
+//
+//     case 4:
+//         console.log('Right!');
+//
+//
+//     case 5:
+//         console.log('Wrong!');
+//
+//
+//     default:
+//         console.log('The result is strange. Really.');
+// }
+
+// let arg = prompt("Enter a value?");
+// switch (arg) {
+//     case 0:
+//     case 1:
+//         console.log( 'One or zero' );
+//         break;
+//
+//     case 2:
+//         console.log( 'Two' );
+//         break;
+//
+//     case 3:
+//         console.log( 'Never executes!' );
+//         break;
+//     default:
+//         console.log( 'An unknown value' );
+// }
+
+// let browser = "Explorer" ;
+// switch (browser) {
+//
+//     case 'Edge':
+//         console.log( "You've got the Edge!" );
+//         break;
+//
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//         console.log( 'Okay we support these browsers too' );
+//         break;
+//
+//     default:
+//         console.log( 'We hope that this page looks ok!' );
+// }
+
+//
+// let browser = "buga";
+// if (browser == "Edge") {
+//     console.log("You've got the Edge!") }
+// else if (browser == "Chrome"
+//         || browser == "Safari"
+//         || browser == "Firefox"
+//         || browser == 'opera');
+//
+// { console.log ('Okay we support these browsers too');
+// };
+//     else if
+//     console.log( 'We hope that this page looks ok!' );
+// }
+
+// let a = +prompt('a?', '');
+//
+// if (a == 0) {
+//     console.log( 0 );
+// }
+// if (a == 1) {
+//     console.log( 1 );
+// }
+//
+// if (a == 2 || a == 3) {
+//     console.log( '2,3' );
+// }
+//
+// let a= (1);
+// switch ( a ) {
+//     case 0 :
+//         console.log(0);
+//         break;
+//     case  1 :
+//         console.log(0)
+//         break;
+//     case 2 || 3:
+//         console.log( 2.3)
+//         break;
+//     default:
+//         console.log(none)
+// }
